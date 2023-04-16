10eb6efc3e761b6bded451f36de4eb0980c552ae
Junior <eecfredes@gmail.com>
2023-04-16 12:45:34 -0300

    :rocket: app clientes - lidando com erros

--- clientes/views.py
+++ clientes/views.py
@@ -19,11 +19,23 @@ def clientes(request):
         cliente = Cliente.objects.filter(cpf=cpf)
 
         if cliente.exists():
-            return HttpResponse('Cliente já cadastrado')
+            return render(
+                request,
+                'clientes.html',
+                {'nome': nome, 'sobrenome': sobrenome, 'email': email,
+                    'carros': zip(carros, placas, anos)}
+            )
+            # return HttpResponse('Cliente já cadastrado')
 
         if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
-            # TODO: Adicionar mensagens
-            return HttpResponse('Email inválido')
+
+            return render(
+                request,
+                'clientes.html',
+                {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf,
+                    'carros': zip(carros, placas, anos)}
+            )
+            # return HttpResponse('Email inválido')
 
         cliente = Cliente(
             nome=nome,
